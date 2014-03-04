6b5027cd8de5c704d8190d859179bb74a55f494c
Alexander Wang <afwang@lolcomp.wikaba.com>
2014-03-04 12:47:07 -0800

    Added first files to git. Will be creating bare repo on server afterwards

---
+++ src/com/example/calculator/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	enum theOperation {ADD, SUBTRACT, MULT, DIV};
+	
+	private theOperation op;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		//Explicitly set default operation.
+		op = theOperation.ADD;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void setOperation(View v) {
+		TextView operTextView = (TextView)findViewById(R.id.operTextView);
+		Button but = (Button)v;
+		char operation = but.getText().toString().charAt(0);
+		
+		operTextView.setText("" + operation);
+		switch(operation)
+		{
+		case '+': op = theOperation.ADD; break;
+		case '-': op = theOperation.SUBTRACT; break;
+		case '*': op = theOperation.MULT; break;
+		case '/': op = theOperation.DIV; break;
+		}
+	}
+	
+	public void calculate(View v) {
+		EditText oper1EditText;
+		EditText oper2EditText;
+		
+		oper1EditText = (EditText)findViewById(R.id.operand1);
+		oper2EditText = (EditText)findViewById(R.id.operand2);
+		
+		String oper1 = oper1EditText.getText().toString();
+		String oper2 = oper2EditText.getText().toString();
+		
+		TextView resTextView = (TextView)findViewById(R.id.resultTextView);
+		
+		double result = 0;
+		
+		try {
+			double op1 = Double.parseDouble(oper1);
+			double op2 = Double.parseDouble(oper2);
+			switch(op) {
+			case ADD: result = op1 + op2; break;
+			case SUBTRACT: result = op1 - op2; break;
+			case MULT: result = op1 * op2; break;
+			case DIV: result = (op2 != 0) ? op1 / op2 : 0; break;
+			}
+		}
+		catch (NumberFormatException e) {
+			resTextView.setText("One of the operands is not a number!");
+			return;
+		}
+		resTextView.setText("" + result);
+	}
+	
+	public void clear(View v) {	
+		EditText oper1EditText = (EditText)findViewById(R.id.operand1);
+		EditText oper2EditText = (EditText)findViewById(R.id.operand2);
+		TextView operTextView = (TextView)findViewById(R.id.operTextView);
+		TextView resTextView = (TextView)findViewById(R.id.resultTextView);
+		
+		oper1EditText.setText("");
+		oper2EditText.setText("");
+		operTextView.setText(R.string.operation);
+		resTextView.setText(R.string.result);
+	}
+}
